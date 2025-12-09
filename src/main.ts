47c3af3042a621c867d16b26833f23d73ad93d60
muntashir-wahid <muntashir.w.riseuplabs@gmail.com>
2025-12-09 18:01:30 +0600

    added cors setup

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1/');
   app.use(morgan('dev'));
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
